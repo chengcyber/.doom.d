09b7b72b144232ccd5afcab4dd76467b61b911d8
Cheng <liucheng.tech@outlook.com>
2019-03-15 23:29:08 +0800

    :sparkles: show magit git process

--- config.el
+++ config.el
@@ -1,3 +1,11 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here
+
+;; https://github.com/magit/magit/wiki/Tips-and-Tricks#automatically-displaying-the-process-buffer
+(defun auto-display-magit-process-buffer (&rest args)
+  "Automatically display the process buffer when it is updated."
+  (let ((magit-display-buffer-noselect t))
+    (magit-process-buffer)))
+
+(advice-add 'magit-process-insert-section :before #'auto-display-magit-process-buffer)
